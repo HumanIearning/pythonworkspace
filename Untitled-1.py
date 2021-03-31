c77f1e235dcd5ade59bdf7812f37ddc9144e174c
KrAiMaster <kimdh3896@naver.com>
2021-03-31 23:34:05 +0900

    first

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+#제곱은 ** | pow(a,b)
+#or 과 and 는 각각 |,&
+#최댓값은 max(a,b) 최소값은 min(a,b)
+#반올림은 round(a)
+#floor sqrt ceil 은 from math import* 를 했을때 각각 내림, 제곱근, 올림 으로 사용가능
+#from random import* 을 하면 random ,randrange , randint 사용가능 
+#random은 0.0 이상 1.0 미만의 임의의값
+
+print("test")
